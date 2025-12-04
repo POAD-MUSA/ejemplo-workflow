40520be02b2792a0782be026a122d22b3e3f9bf1
emilopezcano <emilopezcano@gmail.com>
2025-12-04 17:31:57 +0100

    penguins example

--- R/renv_code.R
+++ R/renv_code.R
@@ -1,1 +1,11 @@
 ## Implement renv
+
+library(dplyr)
+library(ggplot2)
+library(palmerpenguins)
+
+penguins |> 
+  ggplot(aes(x = flipper_length_mm, y = body_mass_g, color = species)) +
+  geom_point() +
+  theme_minimal()
+
